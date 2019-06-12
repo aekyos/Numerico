193a4f3db9cfd5619191e6a8273b9880d4e2b08c
aekyos <22533437+aekyos@users.noreply.github.com>
2019-06-11 23:01:48 -0300

    Add files via upload

---
+++ lab5.jl
@@ -0,0 +1,129 @@
+###########################Ejercicio 1)
+function f_1(x)
+    f = x
+    return f
+end
+
+function inte_num_comp(f,a,b,N,regla)
+    xj = LinRange(a,b,N+1)                 #hago N+1 puntos (asi quedan N intervalos) x espaciados en [a,b]
+    sx = 0
+    
+    if regla=="trapecio"                 #══════════════════════════════════════ REGLA DEL TRAPECIO
+        println("REGLA DEL TRAPECIO:")
+        
+        for i=1 : N
+            h = (xj[i+1]-xj[i])/N
+            sx = sx + h / 2 * (f(xj[i+1])+f(xj[i]))
+        end
+        
+        
+    elseif regla=="pm"                   #═════════════════════════════════ REGLA DEL PUNTO MEDIO ═╗
+        println("REGLA DEL PUNTO MEDIO:")                                                         #║
+                                                                                                  #║
+        for i=1 : N                                                                               #║
+            pm = (xj[i+1]+xj[i])/2                                                                #║
+            sx = sx + f(pm)*(xj[i+1]-xj[i])                                                       #║
+        end                                                                                       #║
+                                         #═════════════════════════════════════════════════════════╝
+    elseif regla=="simpson"              #══════════════════════════════════════ REGLA DE SIMPSON ═╗        da cualquier cosa esto 
+        if (mod(N,2)==1)
+            println("NO SE PUEDE HACER SIMPSON CON N IMPAR.")
+        else
+            println("REGLA DE SIMPSON:")
+            h = (b-a)/N
+            sx0 = f(a)+f(b)
+            sx1 = 0
+            sx2 = 0 
+
+            for i=1 : N-1
+                x = i*h + a
+                if (mod(i,2)==0)
+                    sx2 = sx2 + f(x)
+                else
+                    sx1 = sx1 + f(x)
+                end
+            end
+            sx = (sx0 + 2 * sx2 + 4 * sx1) * h/3
+        end
+
+    else
+        println("no esta esta regla.")
+    end
+    
+    return sx
+end
+
+###########################Ejercicio 2)
+function f_ej2(x)
+    f = 1/exp(x)
+    return f
+end
+
+function ej2()
+    trap_4 = inte_num_comp(f_ej2,0,1,4,"trapecio")
+    trap_10 = inte_num_comp(f_ej2,0,1,10,"trapecio")
+    trap_20 = inte_num_comp(f_ej2,0,1,20,"trapecio")
+    
+    println("La regla del trapecio con 4 intervalos es: ",trap_4)
+    println("La regla del trapecio con 10 intervalos es: ",trap_10)
+    println("La regla del trapecio con 20 intervalos es: ",trap_20)
+    
+    pm_4 = inte_num_comp(f_ej2,0,1,4,"pm")
+    pm_10 = inte_num_comp(f_ej2,0,1,10,"pm")
+    pm_20 = inte_num_comp(f_ej2,0,1,20,"pm")
+    
+    println("La regla del punto medio con 4 intervalos es: ",pm_4)
+    println("La regla del punto medio con 10 intervalos es: ",pm_10)
+    println("La regla del punto medio con 20 intervalos es: ",pm_20)
+    
+    simpson_4 = inte_num_comp(f_ej2,0,1,4,"simpson")
+    simpson_10 = inte_num_comp(f_ej2,0,1,10,"simpson")
+    simpson_20 = inte_num_comp(f_ej2,0,1,20,"simpson")
+        
+    println("La regla de simpson con 4 intervalos es: ",simpson_4)
+    println("La regla de simpson con 10 intervalos es: ",simpson_10)
+    println("La regla de simpson con 20 intervalos es: ",simpson_20)
+end
+
+###########################Ejercicio 3)
+using Plots
+
+function senint(xi)
+    n = ceil(10*xi)                                     #CALCULO LA CANTIDAD DE INTERVALOS
+    integral = inte_num_comp(cos,0,xi,n,"pm")           #HAGO LA INTEGRAL CON ESA CANTIDAD DE PUNTOS
+    
+    return integral    
+end
+
+function ej3(x)
+    y = []
+    real = []
+    
+    for j=1 : length(x)
+        append!(y,senint(j))                            #CALCULO senint(xi)
+        append!(real, sin(j))                           #CALCULO sen(xi)
+    end 
+    
+    #return y                                           #DEVUELVO LA INTEGRAL DE TODOS LOS XI QUE ME PASARON
+    
+    plot(x,integral,linewidth=2,title="My Plot")        #GRAFICO SENINT(x)
+    plot!(x,real)                                       #GRAFICO SEN(x)
+end
+
+###########################Ejercicio 4)a)
+function f_ej4a(x)
+    f = x/(exp(x))
+    return f
+end
+
+function ej4a(x)
+    real = 0,264241                                     #valor real la integral               
+    i = 1
+    a = inte_num_comp(f_ej2,0,1,1,"simpson")
+    while ((real-a) < 0.0001)                           #MIENTRAS NO SE CUMPLA EL ERROR
+        a = inte_num_comp(f_ej2,0,1,i,"simpson")        #CALCULO LA INTEGRAL CON i INTERVALOS   
+        i = i+1                                         #AUMENTO EN UNO LA CANTIDAD DE INTERVALOS
+    end
+    
+    return a
+end
---
+++ lab6.jl
@@ -0,0 +1,156 @@
+###########################Ejercicio 1)
+using LinearAlgebra
+
+#A=[[1 8 3];[4 5 6];[7 8 9]]
+#b=[1,5,9]
+
+function sol_tr_sup(A, b)
+    if det(A)==0
+        println("ERROR: LA MATRIZ ES SINGULAR")
+        return 0
+    end 
+    
+    n = size(A,2)    
+    x = ones(n)
+    for i=n : -1 : 1
+        sum = 0
+        for j=i+1: n
+            sum = sum + A[i,j] * x[j]
+        end
+        x[i] = (b[i] - sum) / A[i,i]
+    end
+    
+    return x
+end
+
+function sol_tr_inf(A,b)
+    if det(A)==0
+        println("ERROR: LA MATRIZ ES SINGULAR")
+        return 0
+    end 
+    
+    n = size(A,2)    
+    x = ones(n)
+    for i=1 : n
+        sum = 0
+        for j=1: i-1
+            sum = sum + A[i,j] * x[j]
+        end
+        x[i] = (b[i] - sum) / A[i,i]
+    end
+    
+    return x
+end
+
+###########################Ejercicio 2)a)
+function egauss(A,b)
+    n = size(A,2)
+    U = copy(A)
+    y = copy(b)
+    
+    for k=1 : n-1
+        for i=k+1 : n
+            if (A[k,k] == 0)
+                println("La matriz ya es singular")
+                break
+            end
+            
+            z = A[i,k]/A[k,k]
+            for j=k+1 : n
+                U[i,j] = A[i,j] / (z*A[k,j])
+            end
+            y[i] = b[i] - z*b[k]
+        end
+    end
+    
+    return [U,y]
+end
+
+###########################Ejercicio 2)b)
+function sol_eg(A,b)
+    gauss = egauss(A,b)               #BIEN?????
+    U = gauss[1] 
+    y = gauss[2] 
+    
+    resultado = sol_tr_sup(U,y)
+    
+    return resultado
+end
+
+###########################Ejercicio 3)
+using LinearAlgebra
+function sol_lu(A, b)
+    L,U,p = lu(A)
+    
+    b2  = p*b
+    y = sol_tr_inf(L,b2)
+    x = sol_tr_sup(U,y)
+    
+    return x
+end
+
+###########################Ejercicio 5)
+function jacobi(A,b,err, mit)
+    n = size(A,1)
+    x_k_menos_1 = zeros(n)
+    x = ones(n)
+    
+    k = 1 
+    while (k<= mit)
+        for i=1 :n
+            aux = 0
+            for j=1 : n
+                if j!=i
+                    aux = aux + A[i,j]*x[j]
+                end
+            end
+            x[i] = aux
+        end
+        if norm(x-x_k_menos_1) < err
+            return [x,k]
+        end
+        x_k_menos_1 = x
+        k=k+1
+    end
+    
+    
+    return [x,k]
+end
+
+function gseidel(A,b,err, mit)
+    n = size(A,2)
+    x_k_menos_1 = zeros(n)
+    x = zeros(n)
+    
+    k = 1 
+    while k <= mit
+        for i=1 :n
+            aux = 0
+            for j=1 : n
+                if j!=i
+                    aux = aux + A[i,j]*x[j]
+                end
+            end
+            x[i] = (b[i] - aux) / A[i,i]
+        end
+        if norm(x-x_k_menos_1) < err             # veo si la norma de ||x-x(k-1)|| es menor que el error
+            return [x,k]
+        end
+        x_k_menos_1 = x                          # actualizo el x(k-1)
+        k=k+1
+    end
+    
+    return [x,k]
+end
+
+###########################Ejercicio 6)a)
+function ej6_a()
+    A = [3 1 1;2 6 1;1 1 4]
+    b = [5,9,6]
+    
+    gs = gseidel(A,b,1e-11,100)
+    j = jacobi(A,b,1e-11,100)
+    
+    println(gs)
+    println(j)
+end
